ed12d4946d61f7445878d6e3649743af6c2bccc1
Huỳnh Gia Huy <63111274+huynhgiahuy@users.noreply.github.com>
2022-02-10 13:59:38 +0700

    Add files via upload

---
+++ client/build/precache-manifest.560d7288eeb2af21dc2a06d842eb90ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a814848cb0eb905196583f36a84e939",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c3f2283ef31dc556892",
+    "url": "/static/css/main.aa4c1396.chunk.css"
+  },
+  {
+    "revision": "513d5144a6c77b590a13",
+    "url": "/static/js/2.1b60d270.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/static/js/2.1b60d270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c3f2283ef31dc556892",
+    "url": "/static/js/main.7f307790.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "2f2d5c7a8f1abf877125a6e268fd9807",
+    "url": "/static/media/showcase.2f2d5c7a.jpg"
+  }
+]);
